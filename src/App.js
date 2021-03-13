d73a6a0eb95fda50ab55d9bad1b71f10b0657216
K-tech19 <kennyjean561@gmail.om>
2021-03-12 19:54:25 -0500

    React Charts.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react'
+import BarChart from './components/BarChart'
+// import state variables useState,  useEffect, 
+
+
+
+import axios from 'axios'
 import './App.css';
 
 export default function Main() {
+  
+
+
   return (
     <div className="App">
       <h1>Covid Tracker 2021 😷</h1>
+      <BarChart />
     </div>
   );
 }
