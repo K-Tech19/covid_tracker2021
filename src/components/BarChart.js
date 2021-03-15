dd477167d11b22cac174c998f642e548022262e5
K-tech19 <kennyjean561@gmail.om>
2021-03-15 07:16:53 -0400

    attempting to access api information

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,15 +1,53 @@
-import React from 'react'
-import { Bar } from 'react-chartjs-2'
+import React, { useState, useEffect} from 'react'
+
+import { Line } from 'react-chartjs-2'
+import axios from 'axios'
 
 const BarChart = () =>{
+
+    const [ currentUsValues, setCurrentUsValues] = useState([]);
+    const [hospitalized, setHospitalized] = useState([]);
+    const [ death, setDeath] = useState([]);
+
+    let currentVal = []
+    let beds = []
+    let deathCount = []
+    let dates = []
+
+
+    axios.get("https://api.covidtracking.com/v1/us/current.json")
+    .then(res =>{
+        console.log(res)
+        for(const dataObj of res.data) {
+            dates.push(parseInt(dataObj.dates))
+            beds.push(parseInt(dataObj.hospitalized))
+            deathCount.push(parseInt(dataObj.death))
+        }
+    })
+    .catch(err => {
+        console.log(err)
+    });
+    console.log(beds)
+    console.log(dates)
+    console.log(deathCount)
+
+
+
+
+
+
+
+
+
+
     return (
         <div>
-            <Bar 
+            <Line 
             data={{
                 labels: ['Reds','Blue','Yellow','Green','Purple','Orange'], 
                 datasets: [{
                     label: '# of cases',
-                    data: [19,16,7,4,8,10,1],
+                    data: beds,
                         backgroundColor: [
                             'rgba(255, 99, 132, 0.2)',
                             'rgba(54, 162, 235, 0.2)',
